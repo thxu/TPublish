c0a716870c7a236edc8df07bedcc7c6d845477de
thxu <729468926@qq.com>
2018-06-25 00:27:15 +0800

    API控制器

---
+++ TPublish/TPublish.Web/Controllers/ClientApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TPublish.Web.Controllers
+{
+    public class ClientApiController : Controller
+    {
+        public string Index()
+        {
+            return "hello";
+        }
+    }
+}
