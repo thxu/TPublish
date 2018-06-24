acfeb27b9551a25da6fa3c48e916f96835ce3d23
thxu <729468926@qq.com>
2018-06-25 00:44:12 +0800

    公共方法。

---
+++ TPublish/TPublish.Common/Common.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace TPublish.Common
+{
+    public static class Common
+    {
+        /// <summary>
+        /// 复制所有文件到指定目录
+        /// </summary>
+        /// <param name="srcPath"></param>
+        /// <param name="destPath"></param>
+        public static void CopyDirectoryTo(this string srcPath, string destPath)
+        {
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+            DirectoryInfo dir = new DirectoryInfo(srcPath);
+            FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //获取目录下（不包含子目录）的文件和子目录
+            foreach (FileSystemInfo i in fileinfo)
+            {
+                if (i is DirectoryInfo)     //判断是否文件夹
+                {
+                    if (!Directory.Exists(destPath + "\\" + i.Name))
+                    {
+                        Directory.CreateDirectory(destPath + "\\" + i.Name);   //目标目录下不存在此文件夹即创建子文件夹
+                    }
+                    i.FullName.CopyDirectoryTo(destPath + "\\" + i.Name);    //递归调用复制子文件夹
+                }
+                else
+                {
+                    File.Copy(i.FullName, destPath + "\\" + i.Name, true);      //不是文件夹即复制文件，true表示可以覆盖同名文件
+                }
+            }
+        }
+
+        /// <summary>
+        /// 版本号自动加1
+        /// </summary>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        public static string AddVersion(this string version)
+        {
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                return "1.0.0.1";
+            }
+            string[] tmp = version.Split('.');
+            int val = Convert.ToInt32(tmp[tmp.Length - 1]) + 1;
+            tmp[tmp.Length - 1] = val.ToString();
+            return string.Join(".", tmp);
+        }
+    }
+}
--- TPublish/TPublish.Web/Controllers/ClientApiController.cs
+++ TPublish/TPublish.Web/Controllers/ClientApiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Web.Administration;
+using TPublish.Common;
 
 namespace TPublish.Web.Controllers
 {
@@ -11,6 +13,24 @@ namespace TPublish.Web.Controllers
         public string Index()
         {
             return "hello";
+        }
+
+        public string PublishToIIS()
+        {
+            using (var mgr = new ServerManager())
+            {
+                var site = mgr.Sites.FirstOrDefault(n => n.Name == "test");
+                if (site == null)
+                {
+                    return "";
+                }
+                var lastVersionPath = site?.Applications["/"]?.VirtualDirectories["/"]?.PhysicalPath;
+                string newVersion = lastVersionPath.AddVersion();
+                lastVersionPath.CopyDirectoryTo(newVersion);
+                site.Applications["/"].VirtualDirectories["/"].PhysicalPath = newVersion;
+                mgr.CommitChanges();
+            }
+            return null;
         }
     }
 }
